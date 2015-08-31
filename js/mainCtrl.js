6f51734f65e39e716a32bbcb4b19f6ad8bcff58b
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-08-31 10:27:00 -0600

    GET appears to be working, post needs work

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -5,18 +5,31 @@ app.controller('mainCtrl', function($scope, parseService){
 
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
+  $scope.message = '';
+
+  $scope.getParseData = parseService.getData();
+
+  $scope.messages = $scope.getParseData();
+
+  $scope.postData = parseService.postData($scope.message);
 
 
-
-  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the 
+    //input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
 
 
 
 
   //uncomment this code when your getParseData function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
-})
+  // This goes and gets new data every second, which mimicking a chat room experience.
+  setInterval(function(){
+    $scope.getParseData();
+    console.log($scope.messages);
+  }, 1500)
+
+
+
+
+
+});
